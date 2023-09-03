648c43f386b723067afc081c2531c6b173c9b430
Ryan Bagan <darcknight1980@gmail.com>
2023-09-03 15:12:49 -0700

    made changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import AWS from '../assets/aws.png';
+// import AWS from '../assets/aws.png';
 import CSS from '../assets/css.png';
 import HTML from '../assets/html-5.png';
 import JS from '../assets/java-script.png';
 import Mongo from '../assets//mongodb.png';
 import Node from '../assets/nodejs.png';
 import ReactIcon from '../assets/react.png';
-import Expo from '../assets/expo.png';
-import Ubuntu from '../assets/ubuntu.png';
-import VSCode from '../assets/vscode.png';
+// import Expo from '../assets/expo.png';
+// import Ubuntu from '../assets/ubuntu.png';
+// import VSCode from '../assets/vscode.png';
 import Python from '../assets/python.png';
 import Netlify from '../assets/netlify.png';
 
@@ -24,9 +24,21 @@ const Skills = () => {
         </div>
 
         <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto' src={AWS} alt="AWS Icon" />
             <p className='my-4'>AWS</p>
+          </div> */}
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={JS} alt="JS Icon" />
+            <p className='my-4'>JavaScript</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Python} alt="Python Icon" />
+            <p className='my-4'>Python</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={ReactIcon} alt="React Icon" />
+            <p className='my-4'>ReactJS</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto' src={CSS} alt="CSS Icon" />
@@ -35,10 +47,6 @@ const Skills = () => {
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto' src={HTML} alt="HTML Icon" />
             <p className='my-4'>HTML</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={JS} alt="JS Icon" />
-            <p className='my-4'>JavaScript</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto' src={Mongo} alt="Mongo Icon" />
@@ -52,26 +60,18 @@ const Skills = () => {
             <img className='w-20 mx-auto' src={Netlify} alt="Netlify Icon" />
             <p className='my-4'>Netlify</p>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={Python} alt="Python Icon" />
-            <p className='my-4'>Python</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={ReactIcon} alt="React Icon" />
-            <p className='my-4'>React</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto' src={Expo} alt="Expo Icon" />
             <p className='my-4'>React Expo</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          </div> */}
+          {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto' src={Ubuntu} alt="Ubuntu Icon" />
             <p className='my-4'>Ubuntu</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+          </div> */}
+          {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto' src={VSCode} alt="VSCode Icon" />
             <p className='my-4'>VSCode</p>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,6 +4,7 @@ import Horns from '../assets/Projects/Horns.png';
 import TuneTastic from '../assets/Projects/TuneTastic.png';
 import Whistler1 from '../assets/Projects/Whistler1.png';
 import ChatSensei from '../assets/Projects/ChatSensei.png';
+import Viewer1 from '../assets/Projects/Viewer1.png';
 
 const Work = () => {
   return (
@@ -18,8 +19,27 @@ const Work = () => {
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
           {/* Grid Items */}
+          <div style={{ backgroundImage: `url(${Viewer1})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Viewer
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="https://the-viewer.netlify.app/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                </a>
+                <a href="https://github.com/Ryanb021/viewer">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+
           <div style={{ backgroundImage: `url(${ChatSensei})` }}
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
@@ -38,7 +58,7 @@ const Work = () => {
           </div>
 
           <div style={{ backgroundImage: `url(${Whistler1})` }}
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
@@ -57,7 +77,7 @@ const Work = () => {
           </div>
 
           <div style={{ backgroundImage: `url(${TuneTastic})` }}
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
@@ -76,7 +96,7 @@ const Work = () => {
           </div>
 
           <div style={{ backgroundImage: `url(${CityExplorer})` }}
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
@@ -95,7 +115,7 @@ const Work = () => {
           </div>
 
           <div style={{ backgroundImage: `url(${Horns})` }}
-          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
@@ -112,7 +132,7 @@ const Work = () => {
               </div>
             </div>
           </div>
-          
+
         </div>
       </div>
     </div>
