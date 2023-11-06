a59639f7769db420dda26ec18a43276190e990fc
Ryan Bagan <darcknight1980@gmail.com>
2023-11-05 18:52:43 -0800

    made a small change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
         </div>
         <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
           <div className='sm:text-right text-4xl font-bold'>
-            <p>Hi. I'm Ryan, nice to meet you. Please check out my portfolio.</p>
+            <p>Hi. I'm Ryan, nice to meet you.</p>
           </div>
           <div>
             <p>
